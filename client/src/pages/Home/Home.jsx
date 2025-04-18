328305302d02eeb47a487252d2adf3f6980a8da1
tkute0401 <kutet497@gmail.com>
2025-04-19 00:04:46 +0530

    message

--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -1,48 +1,9 @@
-// // Main Page Component
-// import React from "react";
-// import BlurHeader from "../../components/home/BlurHeader";
-// import HeroSection from "../../components/home/HeroSection";
-// import PropertiesSection from "../../components/home/PropertiesSection";
-// import OwnerServiceBlock from "../../components/home/OwnerServiceBox";
-// import ServiceBlock from "../../components/home/ServiceBlock";
-
-// const Home = () => {
-//   return (
-//     <div className="font-sans bg-gray-900 text-white">
-//       {/* <BlurHeader /> */}
-//       <HeroSection />
-//       <PropertiesSection />
-//       <ServiceBlock 
-//         title="BUY A HOME"
-//         subtitle="Find, Buy & Own Your Dream" 
-//         buttonText="Explore Buying" 
-//       />
-//       <OwnerServiceBlock />
-//       <ServiceBlock 
-//         title="RENT A HOME" 
-//         subtitle="Rental Homes for Everyone." 
-//         buttonText="Explore Renting" 
-//         imageRight={true}
-//       />
-//       <ServiceBlock 
-//         title="BUY PLOTS/LAND" 
-//         subtitle="Residencial & Commercial" 
-//         buttonText="Explore Plots/Land" 
-//       />
-//     </div>
-//   );
-// };
-
-// export default Home;
-
-// Main Page Component
 import React from "react";
 import HeroSection from "../../components/home/HeroSection";
 import PropertiesSection from "../../components/home/PropertiesSection";
 import OwnerServiceBlock from "../../components/home/OwnerServiceBox";
 import ServiceBlock from "../../components/home/ServiceBlock";
 
-// Main Page Component
 const Home = () => {
   return (
     <div className="font-sans bg-gray-900 text-white">
@@ -50,20 +11,23 @@ const Home = () => {
       <PropertiesSection />
       <ServiceBlock 
         title="BUY A HOME"
-        subtitle="Find, Buy & Own Your Dream" 
-        buttonText="Explore Buying" 
+        subtitle="Find, Buy & Own Your"
+        buttonText="Explore Buying"
+        propertyType="Home"
       />
       <OwnerServiceBlock />
       <ServiceBlock 
         title="RENT A HOME" 
-        subtitle="Rental Homes for Everyone." 
+        subtitle="Rental Homes for Everyone" 
         buttonText="Explore Renting" 
         imageRight={true}
+        propertyType="Home"
       />
       <ServiceBlock 
         title="BUY PLOTS/LAND" 
-        subtitle="Residencial & Commercial" 
+        subtitle="Residential & Commercial" 
         buttonText="Explore Plots/Land" 
+        propertyType="Land"
       />
     </div>
   );
