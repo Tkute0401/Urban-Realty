99be5676ece638dadaf70ee19fb92d6874517269
tkute0401 <kutet497@gmail.com>
2025-08-05 14:57:15 +0530

    message

--- server/server.js
+++ server/server.js
@@ -30,7 +30,14 @@ const clientDistDir = path.join('/app/client/dist'); // Updated path
 
 // Middleware
 app.use(cors({
-  origin: true,
+  origin: [
+    'https://urban-realty-production.up.railway.app',
+    'http://localhost:5173',
+    'https://www.squarefooot.com',
+    'https://www.squarefooot.online'
+  ],
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true // If you're using cookies/sessions
 }));
 app.use(express.json({ limit: '50mb' }));
