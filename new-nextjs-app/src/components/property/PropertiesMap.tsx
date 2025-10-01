22d8c1893bcca9390e900fb5b8c347a341949a70
tkute0401 <kutet497@gmail.com>
2025-10-01 15:57:36 +0530

    Fix 4 googlemaps gone mappels in

--- new-nextjs-app/src/components/property/PropertiesMap.tsx
+++ new-nextjs-app/src/components/property/PropertiesMap.tsx
@@ -26,7 +26,7 @@ const PropertiesMap = ({ properties, selectedProperty, onMarkerClick }) => {
   const [mapplsLoaded, setMapplsLoaded] = useState(false);
   
   // Use environment variable from Next.js
-  const mapplsApiKey = process.env.NEXT_PUBLIC_MAPPLS_API_KEY || '82f5c384638d8cfc7d13e310780bae89';
+  const mapplsApiKey ='82f5c384638d8cfc7d13e310780bae89';
   
   // Debug logging
   console.log('🔧 PropertiesMap Debug Info:', {
