adea67d3c0c8e95594ca982743ba621ae1c4542b
tkute0401 <kutet497@gmail.com>
2025-10-20 18:09:09 +0530

    fix(build): add Project model wrapper for Next API (@/models/Project)

---
+++ new-nextjs-app/src/models/Project.ts
@@ -0,0 +1,7 @@
+// Re-export the backend Project Mongoose model for API route usage
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const Project = require('../../../server/models/Project');
+
+export default Project;
+
+
